b9c61041ce147d1f7e884d5b430086c770c50d7b
Mohsen Azimi <mazimi@lyft.com>
2016-06-17 20:14:17 -0700

    Add routing

--- server.tsx
+++ server.tsx
@@ -1,9 +1,8 @@
 /// <reference path="typings/index.d.ts" />
 
 import * as React from 'react';
-import { renderToString } from 'react-dom/server'
-import {Route} from 'react-router';
-import * as webpack from 'webpack';
+import {renderToString} from 'react-dom/server'
+import {Route, Router, match, RouterContext} from 'react-router';
 import * as Express from 'express';
 
 import App from './components/App';
@@ -12,8 +11,24 @@ const PORT = process.env.PORT || 8088;
 
 const app = Express();
 
+const routes = [
+    <Route path='/' component={App} />
+]
+
 app.get('*', (req, res)=> {
-    res.send(renderToString(<App />));
+
+    // TODO: Fix the typings for match
+    match({routes, location: req.url}, (error, redirectLocation, renderProps) => {
+        if (error) {
+            res.status(500).send(error.message);
+        } else if (redirectLocation) {
+            res.redirect(302, redirectLocation.pathname + redirectLocation.search);
+        } else if (renderProps) {
+            res.status(200).send(renderToString(<RouterContext {...renderProps} />))
+        } else {
+            res.status(404).send('Not found');
+        }
+    });
 });
 
 app.listen(PORT, error => {
