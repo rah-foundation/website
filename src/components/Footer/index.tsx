5018f0c7c1b855489f13df6befb923cba8466526
Mohsen Azimi <mazimi@lyft.com>
2016-07-20 22:53:21 -0700

    A little footer work

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -8,14 +8,30 @@ export default class Footer extends React.Component<{}, {}> {
     render() {
         return (
             <footer className={style.footer}>
-                <ul>
-                    <li>
-                        <Link to='/'>{t('homePage')}</Link>
-                    </li>
-                    <li>
-                        <Link to={tUrl('faq')}>{t('faq')}</Link>
-                    </li>
-                </ul>
+                <div className={style.columns}>
+                    <div className={style.column}>
+                        <h3>صفحات دیگر</h3>
+                        <ul>
+                            <li>
+                                <Link to='/'>{t('homePage')}</Link>
+                            </li>
+                            <li>
+                                <Link to={tUrl('faq')}>{t('faq')}</Link>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className={style.column}>
+                        <h3>درباره ما</h3>
+                        <ul>
+                            <li>
+                                <a href={null}>همکاری با ما </a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className={style.column}>
+                        <h3>منابع</h3>
+                    </div>
+                </div>
             </footer>
         );
     }
